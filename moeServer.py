6bd7f2f93c09753877a694a84cc65feffd4b1397
AprocySanae <axdiaoqi220@gmail.com>
2013-08-20 23:56:59 +0000

    fix some bugs

--- moeServer.py
+++ moeServer.py
@@ -1,10 +1,12 @@
 from twisted.internet import reactor,protocol
 import struct
 
+
 class remoteFacory(protocol.ClientFactory):
     def __init__(self,socks5):
         self.protocol = remoteProtocol
         self.socks5 = socks5
+
     def clientConnectionFailed(self,connector,reason):
         print 'failed:',reason.getErrorMessage()
         self.socks5.transport.loseConnection()
@@ -14,17 +16,24 @@ class remoteFacory(protocol.ClientFactory):
         self.socks5.transport.lostConnection()
 
 class remoteProtocol(protocol.Protocol):
-    def connectMade(self):
+    def connectionMade(self):
         self.socks5 = self.factory.socks5
-        self.socks4.remote = self.transport
-        self.socks4.state = 'transport'
+        self.socks5.remote = self.transport
+        self.socks5.state = 'connecting'
+    # send local the remote data
     def dataReceived(self,data):
         self.socks5.transport.write(data)
 
+    # if remote host lost.Reset the state
+    # and close the connection
+    def connectionLost(self):
+        self.socks5.state = 'listen'
+        self.socks5.transport.lostConnection()
 
 class socks5Protocol(protocol.Protocol):
     def connectionMade(self):
         self.state = 'listen'
+
     def dataReceived(self,data):
         method = getattr(self,self.state)
         method(data)
@@ -33,7 +42,7 @@ class socks5Protocol(protocol.Protocol):
         if data:
             self.transport.write('res')
             self.state = 'accept'
-        self.transport.lostConcection()
+        self.transport.lostConnection()
 
     def accept(self,data):
         (checkPoint,lenth) = struct.unpack('!BB',data[:2])
@@ -42,6 +51,7 @@ class socks5Protocol(protocol.Protocol):
         if checkPoint != 0:
             self.transport.loseConnection()
 
+        ## pickle the data
         destFormat = '!{0}sL'.format(lenth)
         ## 2:2+lenth is host and 2+lenth:6+lenth is port
         (host,port) = struct.unpack(hostFormat,data[2:6+lenth])
@@ -51,7 +61,7 @@ class socks5Protocol(protocol.Protocol):
         factory = remoteFactory(self)
         reactor.connectTCP(host,port,factory)
 
-    def transport(self,data):
+    def connecting(self,data):
         self.remote.write(data)
 
 
